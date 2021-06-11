d087a420bb0898085282c80c968917f6a7df9a0c
Кирилл Кириленко <k.kirilenko@lucky.online>
2021-06-12 00:39:31 +0300

    Initial Commit

---
+++ boostCamp/boostCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  boostCampApp.swift
+//  boostCamp
+//
+//  Created by godemodegame on 12.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct boostCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
